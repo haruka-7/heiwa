2a204ccfcab2961ed8305819bc1051514c2ae43c
Hidrile <hidrile@hyperion-web.fr>
2023-06-30 10:35:32 +0200

    add hash and verify unit test

--- src/services/authors.rs
+++ src/services/authors.rs
@@ -37,12 +37,7 @@ pub fn create_author(
     connection: &mut PgConnection,
     mut new_author: NewAuthor,
 ) -> QueryResult<Author> {
-    let salt: SaltString = SaltString::generate(&mut OsRng);
-    let argon2: Argon2 = Argon2::default();
-    new_author.password = argon2
-        .hash_password(new_author.password.as_ref(), &salt)
-        .unwrap()
-        .to_string();
+    new_author.password = hash_password(&new_author.password);
     insert_into(authors::table)
         .values(&new_author)
         .returning(Author::as_returning())
@@ -67,4 +62,25 @@ pub fn verify_password(password: String, password_hash: &str) -> bool {
     Argon2::default()
         .verify_password(password.as_ref(), &parsed_hash)
         .is_ok()
+}
+
+fn hash_password(password: &String) -> String {
+    let salt: SaltString = SaltString::generate(&mut OsRng);
+    let argon2: Argon2 = Argon2::default();
+    argon2
+        .hash_password(password.as_ref(), &salt)
+        .unwrap()
+        .to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_hash_and_verify_password() {
+        let password: String = "thee michel gun elephant".to_string();
+        let password_hash: String = hash_password(&password);
+        assert!(verify_password(password, password_hash.as_ref()))
+    }
 }
