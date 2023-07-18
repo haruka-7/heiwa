10aa51189f19717119c1c17a2a1a8edad514ba6c
Hidrile <hidrile@hyperion-web.fr>
2023-07-19 01:08:22 +0200

    build fix

--- src/handlers/home.rs
+++ src/handlers/home.rs
@@ -1,5 +1,7 @@
 use crate::templates::HomeTemplate;
 
 pub async fn show() -> HomeTemplate {
-    HomeTemplate { name: "Minako".to_string() }
+    HomeTemplate {
+        name: "Minako".to_string(),
+    }
 }
