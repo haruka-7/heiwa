e89f225869003b529a64fd6585e0ad490d694d02
Hidrile <hidrile@hyperion-web.fr>
2023-08-12 01:56:52 +0200

    templating and add heiwa color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./templates/**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        heiwa: {
+          50: '#FFFFFF',
+          100: '#C9D1D9',
+          200: '#8B949E',
+          300: '#30363D',
+          400: '#161B22',
+          500: '#0D1117',
+          600: '#238636',
+          700: '#207C32',
+        },
+      }
+    },
   },
   plugins: [
     require('@tailwindcss/typography'),
