958205c86a2f48ac3a86d80903377cc6e0d85195
Hidrile <hidrile@hyperion-web.fr>
2023-08-18 11:55:23 +0200

    cli change

--- src/cli/init.rs
+++ src/cli/init.rs
@@ -1,4 +1,5 @@
-use std::fs;
+use std::fs::{create_dir_all, File};
+use std::io::Write;
 use std::io::Result;
 
 pub fn init(project_name: String) {
@@ -8,12 +9,14 @@ pub fn init(project_name: String) {
 }
 
 fn create_config(project_name: &String) -> Result<()> {
+    let mut f = File::create(format!("{}/config.tml", project_name))?;
+    f.write_all(&1234_u32.to_be_bytes())?;
     Ok(())
 }
 
 fn create_dir(project_name: &String) -> Result<()> {
-    fs::create_dir_all(format!("{}/pages", project_name))?;
-    fs::create_dir_all(format!("{}/themes/shizen", project_name))?;
-    fs::create_dir_all(format!("{}/medias", project_name))?;
+    create_dir_all(format!("{}/pages", project_name))?;
+    create_dir_all(format!("{}/themes/shizen", project_name))?;
+    create_dir_all(format!("{}/medias", project_name))?;
     Ok(())
 }
