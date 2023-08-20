7a4167797a585ce0990aeeb5dda701d786946a00
Hidrile <hidrile@hyperion-web.fr>
2023-08-20 19:29:35 +0200

    add html minification

--- src/handlers/page.rs
+++ src/handlers/page.rs
@@ -4,6 +4,7 @@ use axum::response::Html;
 use std::sync::Arc;
 use tera::Context;
 use pulldown_cmark::{Parser, Options, html};
+use minify_html::{Cfg, minify};
 
 pub async fn show(State(state): State<Arc<AppState>>) -> Html<String> {
     let markdown_input = "Hello world, this is a ~~complicated~~ *very simple* example.";
@@ -18,5 +19,14 @@ pub async fn show(State(state): State<Arc<AppState>>) -> Html<String> {
     let mut context = Context::new();
     context.insert("title", "Title");
     context.insert("content", html_output.as_str());
-    Html(state.tera.render("page.html", &context).unwrap())
+
+    let html = state.tera.render("page.html", &context).unwrap();
+    let code: &[u8] = html.as_bytes();
+    let mut cfg = Cfg::new();
+    cfg.do_not_minify_doctype = true;
+    cfg.keep_spaces_between_attributes=true;
+    cfg.ensure_spec_compliant_unquoted_attribute_values = true;
+    let minified_html = minify(&code, &cfg);
+
+    Html(String::from_utf8(minified_html).unwrap())
 }
