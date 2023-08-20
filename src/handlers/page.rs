960a507f9b5258595b544ed5f577bcce2d23d094
Hidrile <hidrile@hyperion-web.fr>
2023-08-20 19:11:23 +0200

    add and use markdown parser on page handler

--- src/handlers/page.rs
+++ src/handlers/page.rs
@@ -1,11 +1,22 @@
 use crate::cli::serve::AppState;
-use axum::extract::{Path, State};
+use axum::extract::State;
 use axum::response::Html;
 use std::sync::Arc;
 use tera::Context;
+use pulldown_cmark::{Parser, Options, html};
 
-pub async fn show(Path(path): Path<String>, State(state): State<Arc<AppState>>) -> Html<String> {
+pub async fn show(State(state): State<Arc<AppState>>) -> Html<String> {
+    let markdown_input = "Hello world, this is a ~~complicated~~ *very simple* example.";
+
+    let mut options = Options::empty();
+    options.insert(Options::ENABLE_STRIKETHROUGH);
+    let parser = Parser::new_ext(markdown_input, options);
+
+    let mut html_output = String::new();
+    html::push_html(&mut html_output, parser);
+
     let mut context = Context::new();
-    context.insert("name", format!("Hidrile {}", path).as_str());
-    Html(state.tera.render("home.html", &context).unwrap())
+    context.insert("title", "Title");
+    context.insert("content", html_output.as_str());
+    Html(state.tera.render("page.html", &context).unwrap())
 }
