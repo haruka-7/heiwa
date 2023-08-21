721092dd9bce6ec9f524fa898832057c5c1288f5
Hidrile <hidrile@hyperion-web.fr>
2023-08-22 00:13:35 +0200

    refacto

--- src/handlers/home.rs
+++ src/handlers/home.rs
@@ -1,4 +1,5 @@
 use crate::cli::serve::AppState;
+use crate::utils::template::minify_html;
 use axum::extract::State;
 use axum::response::Html;
 use std::sync::Arc;
@@ -6,9 +7,11 @@ use tera::Context;
 
 pub async fn show(State(state): State<Arc<AppState>>) -> Html<String> {
     let mut context = Context::new();
-    context.insert("meta_title", "Title");
-    context.insert("meta_description", "description");
-    context.insert("site_title", "Site title");
+    context.insert("meta_title", "Meta title");
+    context.insert("meta_description", "Meta description");
+    context.insert("site_title", state.config.title.as_str());
     context.insert("name", "Hidrile");
-    Html(state.tera.render("home.html", &context).unwrap())
+
+    let html = state.tera.render("home.html", &context).unwrap();
+    Html(minify_html(html))
 }
