916602ba0dafa5221b42d6b767d2aced293caa09
Hidrile <hidrile@hyperion-web.fr>
2023-08-29 16:29:07 +0200

    add home.md generation from init command

--- src/cli/init.rs
+++ src/cli/init.rs
@@ -7,10 +7,14 @@ pub fn init(project_name: String) {
     create_dir_all(format!("{}/pages", project_name)).unwrap();
     create_dir_all(format!("{}/themes", project_name)).unwrap();
 
-    let mut f = File::create(format!("{}/config.toml", project_name)).unwrap();
+    let mut config_file: File = File::create(format!("{}/config.toml", project_name)).unwrap();
     let config: Config = Config::default();
     let config_string: String = toml::to_string(&config).unwrap();
-    f.write_all(config_string.as_bytes()).unwrap();
+    config_file.write_all(config_string.as_bytes()).unwrap();
+
+    let mut home_file: File = File::create(format!("{}/pages/home.md", project_name)).unwrap();
+    let content = b"---\ntitle:\nauthor:\ndate:\npublished:\ndescription:\ntags:\n---\n# Welcome { .text-3xl }\nYou can edit this header by modifiyng the file `home.md`";
+    home_file.write_all(content).unwrap();
 
     println!(
         "Project successfully created!\nNext steps : \n- download a theme in the \"themes\" directory\n- update the config.toml file\n- inside your project, launch the command : \"heiwa serve\"\nEnjoy!"
